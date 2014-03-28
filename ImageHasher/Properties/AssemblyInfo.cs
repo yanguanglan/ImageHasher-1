3cc7849fc347bcc9060ac5330c819375996f3853
Ray Sohn <ray@okcupid.com>
2014-03-28 18:19:56 -0400

    nuget info

--- ImageHasher/Properties/AssemblyInfo.cs
+++ ImageHasher/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageHasher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for hashing images and comparing those hashes for similarity.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Ray Sohn")]
 [assembly: AssemblyProduct("ImageHasher")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © Ray Sohn 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
